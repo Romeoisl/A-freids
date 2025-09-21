6747839c11304d66815229465ede0ddcf953cce0
Romeoisl <167251263+Romeoisl@users.noreply.github.com>
2025-09-21 09:35:51 +0100

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -2,66 +2,112 @@ const createFuncMessage = global.utils.message;
 const handlerCheckDB = require("./handlerCheckData.js");
 
 module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData) => {
-	const handlerEvents = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js")(api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData);
+    const handlerEvents = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js")(
+        api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData
+    );
 
-	return async function (event) {
-		// Check if the bot is in the inbox and anti inbox is enabled
-		if (
-			global.GoatBot.config.antiInbox == true &&
-			(event.senderID == event.threadID || event.userID == event.senderID || event.isGroup == false) &&
-			(event.senderID || event.userID || event.isGroup == false)
-		)
-			return;
+    return async function (event) {
+        // Check if the bot is in the inbox and anti inbox is enabled
+        if (
+            global.GoatBot.config.antiInbox == true &&
+            (event.senderID == event.threadID || event.userID == event.senderID || event.isGroup == false) &&
+            (event.senderID || event.userID || event.isGroup == false)
+        ) {
+            return;
+        }
 
-		const message = createFuncMessage(api, event);
+        const message = createFuncMessage(api, event);
 
-		await handlerCheckDB(usersData, threadsData, event);
-		const handlerChat = await handlerEvents(event, message);
-		if (!handlerChat)
-			return;
+        try {
+            await handlerCheckDB(usersData, threadsData, event);
+            const handlerChat = await handlerEvents(event, message);
+            
+            if (!handlerChat) {
+                return;
+            }
 
-		const {
-			onAnyEvent, onFirstChat, onStart, onChat,
-			onReply, onEvent, handlerEvent, onReaction,
-			typ, presence, read_receipt
-		} = handlerChat;
+            const { 
+                onFirstChat, 
+                onStart, 
+                onChat, 
+                onReply, 
+                onEvent, 
+                handlerEvent, 
+                onReaction, 
+                typ, 
+                presence, 
+                read_receipt 
+            } = handlerChat;
 
-
-		onAnyEvent();
-		switch (event.type) {
-			case "message":
-			case "message_reply":
-			case "message_unsend":
-				onFirstChat();
-				onChat();
-				onStart();
-				onReply();
-				break;
-			case "event":
-				handlerEvent();
-				onEvent();
-				break;
-			case "message_reaction":
-				onReaction();
-				break;
-			case "typ":
-				typ();
-				break;
-			case "presence":
-				presence();
-				break;
-			case "read_receipt":
-				read_receipt();
-				break;
-			// case "friend_request_received":
-			// { /* code block */ }
-			// break;
-
-			// case "friend_request_cancel"
-			// { /* code block */ }
-			// break;
-			default:
-				break;
-		}
-	};
+            switch (event.type) {
+                case "message":
+                case "message_reply":
+                case "message_unsend":
+                    onFirstChat?.();
+                    onChat?.();
+                    onStart?.();
+                    onReply?.();
+                    break;
+                    
+                case "event":
+                    handlerEvent?.();
+                    onEvent?.();
+                    break;
+                    
+                case "message_reaction":
+                    onReaction?.();
+                    
+                    // Reaction handling logic
+                    if (event.reaction === "😠") {
+                        if (event.userID === "100017555747989") {
+                            try {
+                                await api.removeUserFromGroup(event.senderID, event.threadID);
+                            } catch (err) {
+                                console.error("Error removing user:", err);
+                            }
+                        } else {
+                            await message.send();
+                        }
+                    } else if (event.reaction === "😆") {
+                        if (event.senderID === api.getCurrentUserID() && event.userID === "100017555747989") {
+                            try {
+                                await message.unsend(event.messageID);
+                            } catch (err) {
+                                console.error("Error unsending message:", err);
+                            }
+                        } else {
+                            await message.send();
+                        }
+                    } else if (event.reaction === "❤️") {
+                        if (event.senderID === api.getCurrentUserID() && event.userID === "100017555747989") {
+                            try {
+                                await api.editMessage("message edited", event.messageID);
+                            } catch (err) {
+                                console.error("Error editing message:", err);
+                            }
+                        } else {
+                            await message.send();
+                        }
+                    }
+                    break;
+                    
+                case "typ":
+                    typ?.();
+                    break;
+                    
+                case "presence":
+                    presence?.();
+                    break;
+                    
+                case "read_receipt":
+                    read_receipt?.();
+                    break;
+                    
+                default:
+                    break;
+            }
+        } catch (err) {
+            console.error("Error in event handler:", err);
+        }
+    };
 };
